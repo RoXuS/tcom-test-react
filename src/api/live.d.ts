9f0a7d42684f21dcbab7845f5fe1d9812baacfb2
Julien ROUSSEAU <jrousseau@doubletrade.com>
2020-11-13 00:06:55 +0100

    chore: delete useless files

--- src/api/live.d.ts
+++
@@ -1,4 +0,0 @@
-import { ItemResponse, Live, LiveResponse } from "../types/live";
-export declare const getLive: (type: Live) => Promise<LiveResponse>;
-export declare const getItem: (itemId: number) => Promise<ItemResponse | null>;
-//# sourceMappingURL=live.d.ts.map
